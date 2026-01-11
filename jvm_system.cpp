5a752273a3f220d5791c56b07352f2fde0a08cd7
AP-JavaCode <megafoh3g@gmail.com>
2026-01-12 02:01:11 +0500

    get set field

---
+++ jvm_system.cpp
@@ -0,0 +1,106 @@
+#include "jvm_system.h"
+
+jfieldID getFieldID(JNIEnv* env, jobject obj, const char* nameField, const char* typeField){
+	jclass classJ = env->GetObjectClass(obj);
+	jfieldID id = env->GetFieldID(classJ, nameField, typeField);
+	return id;
+}
+
+jobject getFieldObject(JNIEnv* env, jobject obj, const char* nameField, const char* typeField){
+	jfieldID id = getFieldID(env, obj, nameField, typeField);
+	jobject res = env->GetObjectField(obj, id);
+	return res;
+}
+
+jboolean getFieldBoolean(JNIEnv* env, jobject obj, const char* nameField){
+	jfieldID id = getFieldID(env, obj, nameField, "Z");
+	jboolean res = env->GetBooleanField(obj, id);
+	return res;
+}
+
+jbyte getFieldByte(JNIEnv* env, jobject obj, const char* nameField){
+	jfieldID id = getFieldID(env, obj, nameField, "B");
+	jbyte res = env->GetByteField(obj, id);
+	return res;
+}
+
+jshort getFieldShort(JNIEnv* env, jobject obj, const char* nameField){
+	jfieldID id = getFieldID(env, obj, nameField, "S");
+	jshort res = env->GetShortField(obj, id);
+	return res;
+}
+
+jint getFieldInt(JNIEnv* env, jobject obj, const char* nameField){
+	jfieldID id = getFieldID(env, obj, nameField, "I");
+	jint res = env->GetIntField(obj, id);
+	return res;
+}
+
+jlong getFieldLong(JNIEnv* env, jobject obj, const char* nameField){
+	jfieldID id = getFieldID(env, obj, nameField, "J");
+	jlong res = env->GetLongField(obj, id);
+	return res;
+}
+
+jfloat getFieldFloat(JNIEnv*env, jobject obj, const char* nameField){
+	jfieldID id = getFieldID(env, obj, nameField, "F");
+	jfloat res = env->GetFloatField(obj, id);
+	return res;
+}
+
+jdouble getFieldDouble(JNIEnv* env, jobject obj, const char* nameField){
+	jfieldID id = getFieldID(env, obj, nameField, "D");
+	jdouble res = env->GetDoubleField(obj, id);
+	return res;
+}
+
+jchar getFieldChar(JNIEnv* env, jobject obj, const char* nameField){
+	jfieldID id = getFieldID(env, obj, nameField, "C");
+	jchar res = env->GetCharField(obj, id);
+	return res;
+}
+
+void setFieldObject(JNIEnv* env, jobject obj, jobject value, const char* nameField, const char* typeField){
+	jfieldID id = getFieldID(env, obj, nameField, typeField);
+	env->SetObjectField(obj, id, value);
+}
+
+void setFieldBoolean(JNIEnv* env, jobject obj, jboolean value, const char* nameField){
+	jfieldID id = getFieldID(env, obj, nameField, "Z");
+	env->SetBooleanField(obj, id, value);
+}
+
+void setFieldByte(JNIEnv* env, jobject obj, jbyte value, const char* nameField){
+	jfieldID id = getFieldID(env, obj, nameField, "B");
+	env->SetByteField(obj, id, value);
+}
+
+void setFieldShort(JNIEnv* env, jobject obj, jshort value, const char* nameField){
+	jfieldID id = getFieldID(env, obj, nameField, "S");
+	env->GetShortField(obj, id);
+}
+
+void setFieldInt(JNIEnv* env, jobject obj, jint value, const char* nameField){
+	jfieldID id = getFieldID(env, obj, nameField, "I");
+	env->SetIntField(obj, id, value);
+}
+
+void setFieldLong(JNIEnv* env, jobject obj, jlong value, const char* nameField){
+	jfieldID id = getFieldID(env, obj, nameField, "J");
+	env->SetLongField(obj, id, value);
+}
+
+void setFieldFloat(JNIEnv* env, jobject obj, jfloat value, const char* nameField){
+	jfieldID id = getFieldID(env, obj, nameField, "F");
+	env->SetFloatField(obj, id, value);
+}
+
+void setFieldDouble(JNIEnv* env, jobject obj, jdouble value, const char* nameField){
+	jfieldID id = getFieldID(env, obj, nameField, "D");
+	env->SetDoubleField(obj, id, value);
+}
+
+void setFieldChar(JNIEnv* env, jobject obj, jchar value, const char* nameField){
+	jfieldID id = getFieldID(env, obj, nameField, "C");
+	env->SetCharField(obj, id, value);
+}
---
+++ jvm_system.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <jni.h>
+
+#ifndef JVM_SYSTEM_H_
+#define JVM_SYSTEM_H_
+
+jfieldID getFieldID(JNIEnv*, jobject, const char*, const char*);
+
+jobject getFieldObject(JNIEnv*, jobject, const char*, const char*);
+jboolean getFieldBoolean(JNIEnv*, jobject, const char*);
+jbyte getFieldByte(JNIEnv*, jobject, const char*);
+jshort getFieldShort(JNIEnv*, jobject, const char*);
+jint getFieldInt(JNIEnv*, jobject, const char*);
+jlong getFieldLong(JNIEnv*, jobject, const char*);
+jfloat getFieldFloat(JNIEnv*, jobject, const char*);
+jdouble getFieldDouble(JNIEnv*, jobject, const char*);
+jchar getFieldChar(JNIEnv*, jobject, const char*);
+
+void setFieldObject(JNIEnv*, jobject, jobject, const char*, const char*);
+void setFieldBoolean(JNIEnv*, jobject, jboolean, const char*);
+void setFieldByte(JNIEnv*, jobject, jbyte, const char*);
+void setFieldShort(JNIEnv*, jobject, jshort, const char*);
+void setFieldInt(JNIEnv*, jobject, jint, const char*);
+void setFieldLong(JNIEnv*, jobject, jlong, const char*);
+void setFieldFloat(JNIEnv*, jobject, jfloat, const char*);
+void setFieldDouble(JNIEnv*, jobject, jdouble, const char*);
+void setFieldChar(JNIEnv*, jobject, jchar, const char*);
+
+
+#endif
